eaee4de3fd71882b2c98825f295aa13a1d5b01b0
Alex Swift <alex.jones@jellybaby.com>
2024-09-16 00:56:02 +0100

    Standard padding on header bottom

--- src/components/landing/header.js
+++ src/components/landing/header.js
@@ -13,7 +13,7 @@ import {
 
 export default function LandingHeader() {
     return (
-        <div className="flex flex-col max-w-screen overflow-hidden sticky top-0 z-[1000]">
+        <div className="flex flex-col max-w-screen overflow-hidden sticky top-0 z-[1000] pb-[2rem]">
             <div className="h-[80px] bg-custom-bpink flex items-center justify-between py-[4rem] px-[2rem]">
                 <div className="flex gap-2 items-center">
                     <img src={'/assets/logo/longnobg.png'} className="h-16 w-full"/>
